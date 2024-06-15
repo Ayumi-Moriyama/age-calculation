946ed5d599a8c8f9cbb94441438696cfc72c682a
Ayumi-Moriyama <abracadabra01866@gmail.com>
2024-06-15 23:30:27 +0900

    本の通り作った

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <Calc v-bind:title="message" v-on:result-event="appAction"/> 
+    <div class="mt-3 text-left">
+      <table class="table" v-html="log"></table>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <button class="btn btn-danger" v-on:click="doClear">Clear Log</button>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calc
+  },
+  data() {
+    return {
+      message: 'CALC',
+      result:[]
+    }
+  },
+  computed: {
+    log() {
+      let table = '<tr><th>Expression</th><th>Value</th></tr>'
+      if (this.result.length > 0) {
+        for (var i in this.result) {
+          table += '<tr><td>' + this.result[i][0] + '</td><th>'
+            + this.result[i][1] + '</th></tr>'
+        }
+      }
+      return table
+    }
+  },
+  created() {
+    let items = localStorage.getItem('log')
+    let logs = JSON.parse(items)
+    if(logs != null){this.result = logs}
+  },
+  methods: {
+    appAction(exp, res) {
+      this.result.unshift([exp, res])
+      var log = JSON.stringify(this.result)
+      localStorage.setItem('log',log)
+    },
+    doClear() {
+      if (confirm('ログをすべて消去します')) {
+        localStorage.removeItem('log')
+        this.result = []
+      }
+    }
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
