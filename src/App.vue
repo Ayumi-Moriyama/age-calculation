c28085545f2890d5ba93f51bd14294541e4ce5b4
Ayumi-Moriyama <abracadabra01866@gmail.com>
2024-07-17 14:37:19 +0900

    年齢計算プログラムに変更

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,85 @@
-<template>
-  <div id="app">
-    <Calc v-bind:title="message" v-on:result-event="appAction"/> 
-    <div class="mt-3 text-left">
-      <table class="table" v-html="log"></table>
-    </div>
-    <div>
-      <button class="btn btn-danger" v-on:click="doClear">Clear Log</button>
-    </div>
-  </div>
-</template>
+<script setup>
+import { ref } from 'vue'
+// const count = ref(0)
+const date = new Date()
+const year = date.getFullYear()
 
-<script>
-import Calc from './components/Calc.vue'
+// getMonthは0からスタートするので今月は+1する
+const month = date.getMonth() +1 
+const day = date.getDate()
 
-export default {
-  name: 'app',
-  components: {
-    Calc
-  },
-  data() {
-    return {
-      message: 'CALC',
-      result:[]
-    }
-  },
-  computed: {
-    log() {
-      let table = '<tr><th>Expression</th><th>Value</th></tr>'
-      if (this.result.length > 0) {
-        for (var i in this.result) {
-          table += '<tr><td>' + this.result[i][0] + '</td><th>'
-            + this.result[i][1] + '</th></tr>'
-        }
-      }
-      return table
-    }
-  },
-  created() {
-    let items = localStorage.getItem('log')
-    let logs = JSON.parse(items)
-    if(logs != null){this.result = logs}
-  },
-  methods: {
-    appAction(exp, res) {
-      this.result.unshift([exp, res])
-      var log = JSON.stringify(this.result)
-      localStorage.setItem('log',log)
-    },
-    doClear() {
-      if (confirm('ログをすべて消去します')) {
-        localStorage.removeItem('log')
-        this.result = []
-      }
-    }
-  }
-}
+const selectedMonth = ref('')
+const selectedDay = ref('')
+const userInput = ref('')
 
 </script>
+
+<template>
+  <p>今日は {{ year }} 年 {{ month }} 月 {{ day }} 日</p>
+  <p>生年月日を入力してください</p>
+  <div>
+    生年月日:
+      {{ userInput }} 年
+      {{ selectedMonth }} 月
+      {{ selectedDay }} 日
+  </div>
+  <div>
+    <input v-model.number="userInput" placeholder="生まれた年を数字で入力" />
+
+    <select v-model.number="selectedMonth">
+      <option disabled value="">月</option>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+      <option>9</option>
+      <option>10</option>
+      <option>11</option>
+      <option>12</option>
+    </select>
+
+    <select v-model.number="selectedDay">
+      <option disabled value="">日</option>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+      <option>7</option>
+      <option>8</option>
+      <option>9</option>
+      <option>10</option>
+      <option>11</option>
+      <option>12</option>
+      <option>13</option>
+      <option>14</option>
+      <option>15</option>
+      <option>16</option>
+      <option>17</option>
+      <option>18</option>
+      <option>19</option>
+      <option>20</option>
+      <option>21</option>
+      <option>22</option>
+      <option>23</option>
+      <option>24</option>
+      <option>25</option>
+      <option>26</option>
+      <option>27</option>
+      <option>28</option>
+      <option>29</option>
+      <option>30</option>
+      <option>31</option>
+    </select>
+  </div>
+  
+  <p>現在の年齢：</p>
+  <p>次の誕生日まであと</p>
+  <!-- <button @click="count++">Count is: {{ count }}</button> -->
+</template>
+
